1bf024778462fd0898f66f2870e3a8821fbe8530
Johnson Phanyasaeng <jphanyasaeng@gmail.com>
2015-02-27 14:30:25 -0500

    initial commit

---
+++ MedixCollege/Controllers/CareerServicesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MedixCollege.Controllers
+{
+    public class CareerServicesController : Controller
+    {
+        // GET: CareerServices
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: MedixAdvantage
+        public ActionResult MedixAdvantage()
+        {
+            return View();
+        }
+    }
+}
