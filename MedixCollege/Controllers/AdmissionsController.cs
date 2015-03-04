8fafbee1af5dda2cbb99406966b5d214b99197e2
Johnson Phanyasaeng <jphanyasaeng@gmail.com>
2015-03-04 11:01:13 -0500

    initial commit

--- MedixCollege/Controllers/AdmissionsController.cs
+++ MedixCollege/Controllers/AdmissionsController.cs
@@ -13,5 +13,53 @@ namespace MedixCollege.Controllers
         {
             return View();
         }
+
+        // GET: EmploymentOntario
+        public ActionResult EmploymentOntario()
+        {
+            return View();
+        }
+
+        // GET: LifelongLearningPlan
+        public ActionResult LifelongLearningPlan()
+        {
+            return View();
+        }
+
+        // GET: OSAP
+        public ActionResult OSAP()
+        {
+            return View();
+        }
+
+        // GET: OutOfProvinceStudents
+        public ActionResult OutOfProvinceStudents()
+        {
+            return View();
+        }
+
+        // GET: RegisteredEducationSavingsPlans
+        public ActionResult RegisteredEducationSavingsPlans()
+        {
+            return View();
+        }
+
+        // GET: SecondCareers
+        public ActionResult SecondCareers()
+        {
+            return View();
+        }
+
+        // GET: StudentLinesOfCredit
+        public ActionResult StudentLinesOfCredit()
+        {
+            return View();
+        }
+
+        // GET: FinancialOptions
+        public ActionResult FinancialOptions()
+        {
+            return View();
+        }
     }
 }
---
+++ MedixCollege/Controllers/MedixOnlineController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MedixCollege.Controllers
+{
+    public class MedixOnlineController : Controller
+    {
+        // GET: MedixOnline
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Schedules And Fees
+        public ActionResult SchedulesAndFees()
+        {
+            return View();
+        }
+
+        // GET: ELearning
+        public ActionResult ELearning()
+        {
+            return View();
+        }
+
+        // GET: Advantages
+        public ActionResult Advantages()
+        {
+            return View();
+        }
+
+    }
+}
