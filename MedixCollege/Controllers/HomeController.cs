5873027cea3197a9c4d1b9e7578ada1e412024f2
Johnson Phanyasaeng <jphanyasaeng@gmail.com>
2015-03-26 18:58:37 -0400

    v4 tasks and first pass at news

--- MedixCollege/Controllers/HomeController.cs
+++ MedixCollege/Controllers/HomeController.cs
@@ -22,6 +22,46 @@ namespace MedixCollege.Controllers
             return View();
         }
 
+        public ActionResult NewsDirectory()
+        {
+            return View();
+        }
+
+        public ActionResult NewsArticle(int id = 0)
+        {
+            ViewBag.Title = "Fran Welsh from Medix College Brantford wins the Woman of Excellence Award for Active Living and Wellness";
+
+            ViewBag.NewsDate = "March 2, 2015";
+
+            ViewBag.NewsBody = "We would like to congratulate Fran Welsh our Placement Supervisor for winning the Woman of " +
+
+           "Excellence Award for Active Living and Wellness. Welsh has operated a fitness and wellness " +
+
+            "consulting service for 20 years. She is known throughout the city and county for rehabilitation, " +
+
+            "sport­specific training, health, fitness and wellness. She has been a columnist for Fit Brant " +
+
+            "Magazine and a fitness expert on Rogers TV. Welsh is a placement co­ordinator and supervisor " +
+
+            "at Medix College, and works with seniors and people with disabilities at the Boys' and Girls' " +
+
+            "Club. She consults with clients of all ages and fitness levels to develop programs to help them " +
+
+            "optimize health, recover from illness, prepare and recover from injury or surgery, prepare for " +
+
+            "competition or address physical challenges or disabilities. Welsh consults with organizations " +
+
+            "and fitness centres, nationally and internationally, to develop policies, procedures and plans " +
+
+            "ensuring the appropriate purchase of equipment, optimal layout of workout areas and training of " +
+
+            "fitness staff. Clients have included the Department of National Defence, Red Cross and a host " +
+
+            "of fitness centres across Canada and the U.S.";
+
+            return View();
+        }
+
         [HttpPost]
         public async Task<ActionResult> ApplyNow(FormCollection fc)
         {
